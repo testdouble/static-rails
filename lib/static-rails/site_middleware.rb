818203c0dbb18d332e6f43b829b90f26da7ac9f4
Justin Searls <searls@gmail.com>
2020-05-12 22:42:17 +0900

    document a confusing line of code

--- lib/static-rails/site_middleware.rb
+++ lib/static-rails/site_middleware.rb
@@ -18,6 +18,29 @@ module StaticRails
       return @app.call(env) unless @determines_whether_to_handle_request.call(env)
 
       if require_csrf_before_processing_request? && !csrf_token_is_set?(env)
+        # You might be asking yourself what the hell is going on here. In short,
+        # This middleware sits at the top of the stack, which is too early to
+        # set a CSRF token in a cookie. Therefore, we've placed a subclass of
+        # this middleware named SitePlusCsrfMiddleware near the bottom of the
+        # middleware stack, which is slower but comes after Session::CookieStore
+        # and therefore can write _csrf_token to the cookie. As a result, the
+        # observable behavior to the user is identical, but the first request
+        # to set the cookie will be marginally slower because it needs to go
+        # deeper down the Rails middleware stack
+        #
+        # But! Between these two is ActionDispatch::Static. In the odd case that
+        # a path that this middleware would serve happens to match the name of
+        # a path in public/, kicking down the middleware stack would result in
+        # that file being served instead of our deeper middleware being called.
+        # So to work around this we're just making the PATH_INFO property so
+        # ugly that there's no chance it'll match anything. When our subclass
+        # gets its shot at this request, it'll know to remove the path
+        # obfuscation from PATH_INFO and go about its business.
+        #
+        # See, easy!
+        #
+        # (By the way, this was all Matthew Draper's bright idea. You can
+        # compliment him here: https://github.com/matthewd )
         @app.call(env.merge("PATH_INFO" => env["PATH_INFO"] + PATH_INFO_OBFUSCATION))
       elsif StaticRails.config.proxy_requests
         @proxy_middleware.call(env)
