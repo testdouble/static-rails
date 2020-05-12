3155e6466ab82587d9460a2a7766620f293b8a77
Justin Searls <searls@gmail.com>
2020-05-12 12:00:58 +0900

    Add a failing test for CSRF protection help

--- example/app/controllers/docs/api/houses_controller.rb
+++ example/app/controllers/docs/api/houses_controller.rb
@@ -1,10 +1,21 @@
 module Docs
   module Api
     class HousesController < ApplicationController
-      def show
+      def create
         render json: {
-          id: params[:id]
+          id: 22
         }
+      end
+
+      def show
+        if params[:id] == 22
+          render json: {
+            id: 22,
+            fake: true
+          }
+        else
+          head :internal_server_error
+        end
       end
     end
   end
--- example/cypress/integration/static-rails.js
+++ example/cypress/integration/static-rails.js
@@ -37,10 +37,13 @@ describe('rails-static stuff seems to work', () => {
     cy.get('article').should('contain.text', 'I am a post')
   })
 
-  it('can access an API carved out within where the 11ty site is mounted', () => {
-    cy.request('http://blog.localhost:3009/docs/api/houses/22').should((response) => {
-      expect(response.status).to.eq(200)
-      expect(response.body).to.have.property('id', '22')
+  it.only('can access an API carved out within where the 11ty site is mounted', () => {
+    cy.request('POST', 'http://blog.localhost:3009/docs/api/houses').then((res) => {
+      cy.request(`http://blog.localhost:3009/docs/api/houses/${res.body.id}`).should((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body).to.have.property('id', '22')
+        expect(response.body).to.have.property('fake', true)
+      })
     })
   })
 })
