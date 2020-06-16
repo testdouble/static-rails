9617698f53eeca1148a9a789eb4d25cd7c2000a6
Justin Searls <searls@gmail.com>
2020-06-16 09:49:14 +0900

    dynamically generate the methods we can. other 3 are mysteriously failing when doing so

--- lib/static-rails/gets_csrf_token.rb
+++ lib/static-rails/gets_csrf_token.rb
@@ -10,16 +10,18 @@ module StaticRails
 
     private
 
-    def csrf_token_hmac(session, identifier)
-      ActionController::RequestForgeryProtection.instance_method(:csrf_token_hmac).bind(self).call(session, identifier)
-    end
-
-    def mask_token(raw_token)
-      ActionController::RequestForgeryProtection.instance_method(:mask_token).bind(self).call(raw_token)
+    [
+      :csrf_token_hmac,
+      :mask_token,
+      :xor_byte_strings
+    ].each do |method|
+      define_method method do |*args, **kwargs, &blk|
+        ActionController::RequestForgeryProtection.instance_method(method).bind(self).call(*args, **kwargs, &blk)
+      end
     end
 
     def masked_authenticity_token(session, form_options: {})
-      ActionController::RequestForgeryProtection.instance_method(:masked_authenticity_token).bind(self).call(session, form_options)
+      ActionController::RequestForgeryProtection.instance_method(:masked_authenticity_token).bind(self).call(session, form_options: form_options)
     end
 
     def global_csrf_token(session)
@@ -28,10 +30,6 @@ module StaticRails
 
     def real_csrf_token(session)
       ActionController::RequestForgeryProtection.instance_method(:real_csrf_token).bind(self).call(session)
-    end
-
-    def xor_byte_strings(s1, s2)
-      ActionController::RequestForgeryProtection.instance_method(:xor_byte_strings).bind(self).call(s1, s2)
     end
 
     def per_form_csrf_tokens
